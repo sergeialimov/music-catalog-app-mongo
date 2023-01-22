b5f6fc19be7cbda0b2191aa94e50bc2322766b65
Sergei Alimov <dev.sergeialimov@gmail.com>
2023-01-22 07:03:53 +0300

    add mongoose schema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsController } from './songs/songs.controller';
 import { SongsService } from './songs/songs.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/music-app')],
   controllers: [AppController, SongsController],
   providers: [AppService, SongsService],
 })
---
+++ src/libs/db-lib/schemas/song.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type SongDocument = HydratedDocumen<Song>;
+
+@Schema
+export class Song {
+  @Prop()
+  title: string,
+
+  @Prop()
+  author: string,
+
+  @Prop()
+  year: number,
+}
+
+
+export const SongSchema = SchemaFactory.createForClass(Song);
